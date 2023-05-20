296bb1655bbf9af5f5ad4b1ece460428f53841ac
LocalUser <121312523+localuser-isback@users.noreply.github.com>
2023-05-20 09:58:46 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,15 +7,24 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-
+	"path"
+	"time"
+	"bytes"
+	"regexp"
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
 	http.HandleFunc("/fetch", fetchHandler)
 	http.HandleFunc("/proxy", proxyHandler)
+	fs := http.FileServer(http.Dir("static"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	    http.ServeFile(w, r, "static/index.html")
+	})
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
 
 func fetchHandler(w http.ResponseWriter, r *http.Request) {
 	urlParam := r.URL.Query().Get("url")
@@ -31,10 +40,41 @@ func fetchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	currentURL := getCurrentURL(r)
+
+	// Check if the fetched URL is a file
+	ext := path.Ext(urlParam)
+	if ext == ".js" || ext == ".css" {
+		// Remove HTML tags from the file contents
+		re := regexp.MustCompile("<[^>]*>")
+		plainText := re.ReplaceAllString(fetchedHTML, "")
+
+		// Send the plain text as raw text
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		http.ServeContent(w, r, "", time.Time{}, bytes.NewReader([]byte(plainText)))
+		return
+	}
+
+	// Modify HTML links
 	modifiedHTML := modifyHTMLLinks(fetchedHTML, currentURL, urlParam)
 
-	w.Header().Set("Content-Type", "text/html")
+	// Send the modified HTML
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
 	fmt.Fprint(w, modifiedHTML)
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+    // Create a file server that serves static files from the "static" directory
+    fs := http.FileServer(http.Dir("static"))
+
+    // Serve the "index.html" file located in the "static" directory
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "static/index.html")
+    })
+
+    // Serve all other static files from the file server
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
 }
 
 func fetchHTML(urlParam string) (string, error) {
@@ -42,10 +82,14 @@ func fetchHTML(urlParam string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
 
 	client := &http.Client{}
+	req.Header.Set("Access-Control-Allow-Origin", "*")
+	req.Header.Set("Access-Control-Allow-Methods", "*")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
 	resp, err := client.Do(req)
+	contentType := resp.Header.Get("Content-Type")
+	req.Header.Set("Content-Type", contentType)
 	if err != nil {
 		return "", err
 	}
@@ -100,25 +144,115 @@ func modifyHTMLLinks(html, currentURL, originalURL string) string {
 		return resolvedURL.String()
 	}
 
-	doc.Find("a[href], link[href], script[src], img[src]").Each(func(i int, s *goquery.Selection) {
-		if attr, exists := s.Attr("href"); exists {
-			s.SetAttr("href", modifyURL(attr))
-		}
+	doc.Find("a[href], link[href], script[src], img[src], meta[content]").Each(func(i int, s *goquery.Selection) {
+	    if attr, exists := s.Attr("href"); exists {
+	        if s.Is("link") && (s.AttrOr("rel", "") == "icon" || s.AttrOr("rel", "") == "shortcut icon") {
+	            // Handle favicon links
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            proxyURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("href", proxyURL)
+	        } else if s.Is("link") && s.AttrOr("rel", "") == "stylesheet" {
+	            // Handle stylesheet links
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            proxyURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("href", proxyURL)
+	        } else if s.Is("link") && shouldProxyImage(attr) {
+	            // Handle link images
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            proxyURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("href", proxyURL)
+	        } else if s.Is("link") {
+	            // Handle link images
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            fetchURL := fmt.Sprintf("%s/fetch?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("href", fetchURL)
+	        } else {
+	            // Handle other links
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            fetchURL := fmt.Sprintf("%s/fetch?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("href", fetchURL)
+	        }
+	    }
 
-		if attr, exists := s.Attr("src"); exists {
-			if shouldProxyImage(attr) {
-				proxyURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(attr))
-				s.SetAttr("src", proxyURL)
-			} else {
-				modifiedURL := modifyURL(attr)
-				if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
-					modifiedURL = currentScheme + "://" + modifiedURL
-				}
-				s.SetAttr("src", modifiedURL)
-			}
-			s.SetAttr("loading", "lazy") // Add 'loading="lazy"' attribute for lazy loading
-		}
+	    if attr, exists := s.Attr("meta"); exists {
+	    	if s.Is("meta") {
+	    	    // Handle meta images
+	    	    modifiedURL := modifyURL(attr)
+	    	    if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	    	        modifiedURL = currentScheme + "://" + modifiedURL
+	    	    }
+	    	    fetchURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	    	    s.SetAttr("content", fetchURL)
+	        } else {
+	            // Handle other meta links
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            fetchURL := fmt.Sprintf("%s/fetch?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("href", fetchURL)
+	        }
+	    }
+
+	    if attr, exists := s.Attr("src"); exists {
+	        if s.Is("script") && (strings.Contains(attr, "js") || strings.Contains(attr, ".js")) {
+	            // Handle script tags with "js" in the src attribute
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            fetchURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("src", fetchURL)
+	        } else if s.Is("script") {
+	            // Handle other script tags
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            fetchURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("src", fetchURL)
+	        } else if shouldProxyImage(attr) {
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            proxyURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+	            s.SetAttr("src", proxyURL)
+		    } else if s.Is("script") && s.AttrOr("async", "") != "" {
+		        // Handle script tags with "async" attribute
+		        modifiedURL := modifyURL(attr)
+		        if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+		            modifiedURL = currentScheme + "://" + modifiedURL
+		        }
+		        fetchURL := fmt.Sprintf("%s/proxy?url=%s", currentURL, url.QueryEscape(modifiedURL))
+		        s.SetAttr("src", fetchURL)
+		    } else {
+	            modifiedURL := modifyURL(attr)
+	            if !strings.HasPrefix(modifiedURL, "http://") && !strings.HasPrefix(modifiedURL, "https://") {
+	                modifiedURL = currentScheme + "://" + modifiedURL
+	            }
+	            s.SetAttr("src", modifiedURL)
+	        }
+	    }
 	})
+
+
+
 
 	modifiedHTML, err := doc.Html()
 	if err != nil {
@@ -131,7 +265,7 @@ func modifyHTMLLinks(html, currentURL, originalURL string) string {
 
 
 func shouldProxyImage(url string) bool {
-	imageExtensions := []string{".jpg", ".jpeg", ".png", ".gif", ".bmp"}
+	imageExtensions := []string{".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".svg"}
 
 	for _, ext := range imageExtensions {
 		if strings.HasSuffix(strings.ToLower(url), ext) {
@@ -206,6 +340,8 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+	req.Header.Set("Access-Control-Allow-Origin", "*")
+	req.Header.Set("Access-Control-Allow-Methods", "*")
 
 	resp, err := client.Do(req)
 	if err != nil {
